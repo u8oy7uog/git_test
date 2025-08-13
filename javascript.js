b04bcf0d64166101391e16b29ce150c7e6078a83
Viesturs Paegle <viesturspaeglev@gmail.com>
2025-08-13 18:11:09 +0300

    Variables and Operators

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+const pi = 3.14;
+console.log(pi); // What will be output?
+
+console.log((3 + 2) - 76 * (1 + 1))
+console.log(23+97+6+3+5+6+8-92)
+console.log((4 + 6 + 9) / 77)
+let a = 15
+let b = 7 * a
+const max = 57
+const actual = max - 13
+const procentage = actual / max
+console.log(b+a+12)
+console.log(procentage)
