2c6824116e5d9d5371f0b5599386c9faf9541151
Viesturs Paegle <viesturspaeglev@gmail.com>
2025-08-14 22:04:29 +0300

    update

--- javascript.js
+++ javascript.js
@@ -24,3 +24,28 @@ const actual = max - 13
 const procentage = actual / max
 console.log(b+a+12)
 console.log(procentage)
+
+let userName = "John";
+let admin = userName;
+console.log(admin)
+
+const myInt = 5;
+const myFloat = 6.667;
+
+console.log(myInt);   // prints 5
+console.log(myFloat); // prints 6.667
+
+console.log(typeof myInt);   // prints "number"
+console.log(typeof myFloat); // prints "number"
+
+const lotsOfDecimal = 1.7665849587;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(4);
+console.log(twoDecimalPlaces)
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3;
+console.log(myNumber)
+
+let ben = 10
+ben /= 5
+console.log(ben)
